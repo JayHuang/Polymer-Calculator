c2d13ebcf40153996a6bb761ce4c4d2d2b77699d
Jay Huang <JayHuang@users.noreply.github.com>
2014-12-15 16:52:37 -0800

    Added special buttons + simple eval for calculation

--- assets/js/polymer-calculator.js
+++ assets/js/polymer-calculator.js
@@ -1,5 +1,7 @@
 Polymer('polymer-calculator', {
 	theme: 'light',
+	equation: '',
+	result: '25',
 	numpad: [
 		['7', '8', '9', '*', 'CLR'],
 		['4', '5', '6', '/', 'root'],
@@ -7,8 +9,31 @@ Polymer('polymer-calculator', {
 		['.', '0', '=', '-'],
 	],
 
+	clear: function() {
+		this.equation = '';
+		this.result = '';
+	},
+
 	processButton: function(event) {
+		// window.a = this.equation;
+		// window.b = this.result;
 		var val = event.target.attributes['data-value'].value;
 		// console.log(val);
+
+		if(val === 'root') {
+			this.result = Math.sqrt(this.result);
+		} else if (val === 'CLR') {
+			this.equation = '';
+			this.result = '';
+		} else if (val === '=') {
+			this.calculate();
+		} else {
+			this.equation += val;
+		}
+	},
+
+	calculate: function() {
+		// console.log(eval(this.equation));
+		return eval(this.equation);
 	}
 });
